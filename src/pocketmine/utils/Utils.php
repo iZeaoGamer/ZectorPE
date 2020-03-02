da2b713afdb81e2ed619e3493c7bd3db5adb9870
iZeaoGamer <jameshayward14@icloud.com>
2020-03-02 23:12:01 +0000

    Add javaStringHash

--- src/pocketmine/utils/Utils.php
+++ src/pocketmine/utils/Utils.php
@@ -551,6 +551,30 @@ class Utils{
 
 		return $ret;
 	}
+		/**
+	 * @param $string
+	 *
+	 * @return int
+	 */
+	public static function javaStringHash($string){
+		$hash = 0;
+		for($i = 0; $i < strlen($string); $i++){
+			$ord = ord($string{$i});
+			if($ord & 0x80){
+				$ord -= 0x100;
+			}
+			$hash = 31 * $hash + $ord;
+			while($hash > 0x7FFFFFFF){
+				$hash -= 0x100000000;
+			}
+			while($hash < -0x80000000){
+				$hash += 0x100000000;
+			}
+			$hash &= 0xFFFFFFFF;
+		}
+		return $hash;
+	}
+
 	
 	/**
 	 * PUTs data to an url
