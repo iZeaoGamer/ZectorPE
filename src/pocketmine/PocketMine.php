1343fa5a8cb2cd6f5a2cdda963f4434f407ebdad
VMPE Team <jameshayward14@icloud.com>
2020-03-03 10:25:39 +0000

    no no no, not today

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -91,12 +91,6 @@ namespace pocketmine {
 		@define("pocketmine\\PATH", getcwd() . DIRECTORY_SEPARATOR);
 	}
 
-	if(version_compare("7.2", PHP_VERSION) > 0){
-		echo "[CRITICAL] You must use PHP >= 7.2" . PHP_EOL;
-		echo "[CRITICAL] Please use the installer provided on the homepage." . PHP_EOL;
-		exit(1);
-	}
-
 	if(!extension_loaded("pthreads")){
 		echo "[CRITICAL] Unable to find the pthreads extension." . PHP_EOL;
 		echo "[CRITICAL] Please use the installer provided on the homepage." . PHP_EOL;
