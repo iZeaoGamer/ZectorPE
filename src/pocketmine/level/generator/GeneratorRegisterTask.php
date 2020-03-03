143c62e6618ada840edc17041bde7f97333315b7
VMPE Team <jameshayward14@icloud.com>
2020-03-03 14:07:04 +0000

    Fix registeration of Generator tasks

--- src/pocketmine/level/generator/GeneratorRegisterTask.php
+++ src/pocketmine/level/generator/GeneratorRegisterTask.php
@@ -22,43 +22,43 @@
 namespace pocketmine\level\generator;
 
 use pocketmine\block\Block;
+
 use pocketmine\level\generator\biome\Biome;
 use pocketmine\level\Level;
 use pocketmine\level\SimpleChunkManager;
 use pocketmine\scheduler\AsyncTask;
+
 use pocketmine\utils\Random;
 
-class GeneratorRegisterTask extends AsyncTask {
+class GeneratorRegisterTask extends AsyncTask{
 
 	public $generator;
 	public $settings;
 	public $seed;
 	public $levelId;
-	public $waterHeight;
+	protected $yMask;
+	protected $maxY;
 
-	/**
-	 * GeneratorRegisterTask constructor.
-	 *
-	 * @param Level     $level
-	 * @param Generator $generator
-	 */
 	public function __construct(Level $level, Generator $generator){
 		$this->generator = get_class($generator);
-		$this->waterHeight = $generator->getWaterHeight();
 		$this->settings = serialize($generator->getSettings());
 		$this->seed = $level->getSeed();
 		$this->levelId = $level->getId();
+		$this->yMask = $level->getYMask();
+		$this->maxY = $level->getMaxY();
 	}
 
 	public function onRun(){
 		Block::init();
 		Biome::init();
-		$manager = new SimpleChunkManager($this->seed, $this->waterHeight);
+
+		$manager = new SimpleChunkManager($this->seed, $this->yMask, $this->maxY);	
 		$this->saveToThreadStore("generation.level{$this->levelId}.manager", $manager);
 		/** @var Generator $generator */
 		$generator = $this->generator;
 		$generator = new $generator(unserialize($this->settings));
 		$generator->init($manager, new Random($manager->getSeed()));
 		$this->saveToThreadStore("generation.level{$this->levelId}.generator", $generator);
+		
 	}
 }
