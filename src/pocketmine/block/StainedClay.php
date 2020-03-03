ecc273b304567a92642f3b93ae3f5b6bb91fc890
VMPE Team <jameshayward14@icloud.com>
2020-03-03 13:20:45 +0000

    Re-added StainedClay

--- src/pocketmine/block/StainedClay.php
+++ src/pocketmine/block/StainedClay.php
@@ -24,23 +24,54 @@ namespace pocketmine\block;
 
 use pocketmine\item\Tool;
 
-class StainedClay extends Solid{
+class StainedClay extends Solid {
 
 	protected $id = self::STAINED_CLAY;
 
+	const CLAY_WHITE = 0;
+	const CLAY_ORANGE = 1;
+	const CLAY_MAGENTA = 2;
+	const CLAY_LIGHT_BLUE = 3;
+	const CLAY_YELLOW = 4;
+	const CLAY_LIME = 5;
+	const CLAY_PINK = 6;
+	const CLAY_GRAY = 7;
+	const CLAY_LIGHT_GRAY = 8;
+	const CLAY_CYAN = 9;
+	const CLAY_PURPLE = 10;
+	const CLAY_BLUE = 11;
+	const CLAY_BROWN = 12;
+	const CLAY_GREEN = 13;
+	const CLAY_RED = 14;
+	const CLAY_BLACK = 15;
+
+	/**
+	 * StainedClay constructor.
+	 *
+	 * @param int $meta
+	 */
 	public function __construct($meta = 0){
 		$this->meta = $meta;
 	}
 
+	/**
+	 * @return float
+	 */
 	public function getHardness(){
 		return 1.25;
 	}
 
+	/**
+	 * @return int
+	 */
 	public function getToolType(){
 		return Tool::TYPE_PICKAXE;
 	}
 
-	public function getName(){
+	/**
+	 * @return string
+	 */
+	public function getName() : string{
 		static $names = [
 			0 => "White Stained Clay",
 			1 => "Orange Stained Clay",
